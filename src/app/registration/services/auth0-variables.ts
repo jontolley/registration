a956314b3b0839aea700c703c58c4c4d14c807ea
Jon Tolley <jon@tolleyfam.com>
2017-12-31 18:43:10 -0800

    changed callback to https

--- src/app/registration/services/auth0-variables.ts
+++ src/app/registration/services/auth0-variables.ts
@@ -9,6 +9,6 @@ export const AUTH_CONFIG: AuthConfig = {
     clientID: 'otOQqNLsdg41z2xnzed1hbXZQGH2AWXb',
     domain: 'tolleyfam.auth0.com',
     // callbackURL: 'http://encampment.local:4200/register/callback',
-    callbackURL: 'http://sunrise2018.org/register/callback',
+    callbackURL: 'https://sunrise2018.org/register/callback',
     apiUrl: 'https://sunrise2018.org/api'
 };
