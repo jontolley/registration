a9875ef31bf783c68ff56e1d417e24ce04b87867
Jon Tolley <jon@tolleyfam.com>
2018-01-20 13:39:56 -0800

    changed oath callback to sunrise2018.org

--- src/app/registration/services/auth0-variables.ts
+++ src/app/registration/services/auth0-variables.ts
@@ -8,7 +8,7 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
     clientID: 'otOQqNLsdg41z2xnzed1hbXZQGH2AWXb',
     domain: 'tolleyfam.auth0.com',
-    callbackURL: 'http://encampment.local:4200/register/callback',
-    //callbackURL: 'https://sunrise2018.org/register/callback',
+    //callbackURL: 'http://encampment.local:4200/register/callback',
+    callbackURL: 'https://sunrise2018.org/register/callback',
     apiUrl: 'https://sunrise2018.org/api'
 };
