bd56720a62e5a9275dbe531eea659f3c5aa3a291
Jon Tolley <jon@tolleyfam.com>
2018-07-18 21:59:36 -0700

    updates for merit badge prereq and schedule chagnes

--- src/app/pages/schedule/schedule.component.ts
+++ src/app/pages/schedule/schedule.component.ts
@@ -36,9 +36,10 @@ export class ScheduleComponent implements OnInit {
         { time:'1:00 pm',title:'Camp-Wide Flag Ceremony & Opening'},
         { time:'2:00 pm',title:'OPEN: Activities / Merit Badge Midway / Trading Post'},
         { time:'5:00 pm',title:'CLOSE: Activities / Merit Badge Midway / Trading Post'},
-        { time:'5:15 pm',title:'DINNER / Pick up Food'},
+        { time:'5:15 pm',title:'DINNER'},
         { time:'7:00 pm',title:'Special Event'},
         { time:'8:00 pm',title:'Opening Ceremony'},
+        { time:'9:45 pm',title:'Stake Presidents meet at staff area'},
         { time:'10:00 pm',title:'Prepare for sleep'},
         { time:'11:00 pm',title:'Quiet'}
       ]
@@ -49,18 +50,18 @@ export class ScheduleComponent implements OnInit {
       events: [
         { time:'6:00 am',title:'Rise, Prepare for Day'},
         { time:'6:30 am',title:'Morning Spiritual Preparation'},
-        { time:'6:30 am',title:'Stake Presidents meet at staff area'},
-        { time:'7:15 am',title:'BREAKFAST / Pick up Food'},
+        { time:'7:15 am',title:'BREAKFAST'},
         { time:'8:45 am',title:'Clean up and prepare for activities'},
         { time:'9:00 am',title:'OPEN: Activities / Merit Badge Midway / Trading Post/Service Outings'},
         { time:'12:00 pm',title:'CLOSE: Activities / Merit Badge Midway / Trading Post'},
-        { time:'12:00 pm',title:'LUNCH / Pick up Food'},
+        { time:'12:00 pm',title:'LUNCH'},
         { time:'',title:'	Special Events'},
         { time:'1:30 pm',title:'OPEN: Activities / Merit Badge Midway / Trading Post/Service Outings'},
         { time:'5:00 pm',title:'CLOSE: Activities / Merit Badge Midway / Trading Post'},
-        { time:'5:15 pm',title:'DINNER / Pick up Food'},
+        { time:'5:15 pm',title:'DINNER'},
         { time:'7:15 pm',title:'Camp-Wide Flags'},
         { time:'8:00 pm',title:'Tuesday, Thursday: Program with Stake or Ward, Wednesday: Camp wide fireside, Friday: Closing Ceremony'},
+        { time:'9:45 pm',title:'Stake Presidents meet at staff area'},
         { time:'10:00 pm',title:'Prepare for sleep'},
         { time:'11:00 pm',title:'Quiet'}
       ]
@@ -71,8 +72,7 @@ export class ScheduleComponent implements OnInit {
       events: [
         { time:'6:00 am',title:'Rise, Prepare for Day'},
         { time:'6:30 am',title:'Scripture Study'},
-        { time:'6:30 am',title:'Stake Presidents meet at staff area'},
-        { time:'7:15 am',title:'BREAKFAST / Pick up Food'},
+        { time:'7:15 am',title:'BREAKFAST'},
         { time:'8:00 am',title:'Clean up Campsite & Activity Stations & prepare to leave at scheduled time',
         subevents1:[
           {time:'8:00 am', title:'Frenchtown'},
